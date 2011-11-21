d449c670f654af566b67fb1ce68e7072d50cf404
SM <sol@archetyped.com>
2011-11-21 22:21:04 +0000

    Update version

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 Plugin Name: Favicon Rotator
 Plugin URI: http://archetyped.com/lab/fir-1-2-beta/
 Description: Easily set site favicon and even rotate through multiple icons
-Version: 1.2
+Version: 1.2.1
 Author: Archetyped
 Author URI: http://archetyped.com
 */
